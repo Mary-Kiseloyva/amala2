923082a29c3a79fc40b25ab5a11d559104a730b3
Mary-Kiseloyva <Yu.mariyu03@yandex.ru>
2023-04-25 11:22:56 +0300

    hehe

---
+++ src/main/java/com/example/amala2/Amala2Application.java
@@ -0,0 +1,13 @@
+package com.example.amala2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amala2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Amala2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amala2/Amala2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amala2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amala2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
